238c5376670eff50cd6ab0bd7d1176fc236126de
Dylan <40635921+robothomas@users.noreply.github.com>
2020-02-16 11:40:05 -0600

    Change spaces to tabs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,99 +18,99 @@ import edu.wpi.first.wpilibj.util.Units;
  */
 public final class Constants {
 
-  // Port Numbers
+	// Port Numbers
 
-  // Drive Ports
+	// Drive Ports
 	public static final int kLeftFrontPort = 1;
 	public static final int kRightFrontPort = 3;
 	public static final int kLeftRearPort = 2;
-  public static final int kRightRearPort = 4;
+	public static final int kRightRearPort = 4;
 
-  // Xbox controller port
-  public static final int kXboxPort = 0;
+	// Xbox controller port
+	public static final int kXboxPort = 0;
 
-  // Spinner port
-  public static final int kSpinnerPort = 10; 
+	// Spinner port
+	public static final int kSpinnerPort = 10; 
 
-  // Compressor port
-  public static final int compressorModule = 20;
+	// Compressor port
+	public static final int compressorModule = 20;
 
-  // Solenoid ports
-  public static final int leftPistonPort = 0;
+	// Solenoid ports
+	public static final int leftPistonPort = 0;
 	public static final int rightPistonPort = 1; 
 
-  // Arm motor ports
-  public static final int leftArmPort = 7;
-  public static final int rightArmPort = 8;
+	// Arm motor ports
+	public static final int leftArmPort = 7;
+	public static final int rightArmPort = 8;
 
-  // Shooter motor ports
-  public static final int kLeftShooterWheelPort = 6;
-  public static final int kRightShooterWheelPort = 5;
+	// Shooter motor ports
+	public static final int kLeftShooterWheelPort = 6;
+	public static final int kRightShooterWheelPort = 5;
 
-  // Conveyor belt motor port
-  public static final int kConveyorBelt = 9;
-  
-  // Solenoid controlling gear ports 
-  public static final int leftGearPort = 2;
-  public static final int rightGearPort = 3;
+	// Conveyor belt motor port
+	public static final int kConveyorBelt = 9;
 
-  // Motor Phase
-  public static final boolean kLeftInverted = false;
-  public static final boolean kRightInverted = true;
+	// Solenoid controlling gear ports 
+	public static final int leftGearPort = 2;
+	public static final int rightGearPort = 3;
 
-  // Encoder Sensor Phase
-  public static final boolean kLeftEncoderPhase = false;
-  public static final boolean kRightEncoderPhase = false;
+	// Motor Phase
+	public static final boolean kLeftInverted = false;
+	public static final boolean kRightInverted = true;
 
-  // Robot Measurements
-  public static final double kTicksPerRev = 4096;
-  public static final double kTrackWidthMeters = Units.inchesToMeters(10);
-  public static final double kGearRatio = 7.29;
-  public static final double kWheelRadiusMeters = Units.inchesToMeters(3.0);
+	// Encoder Sensor Phase
+	public static final boolean kLeftEncoderPhase = false;
+	public static final boolean kRightEncoderPhase = false;
+
+	// Robot Measurements
+	public static final double kTicksPerRev = 4096;
+	public static final double kTrackWidthMeters = Units.inchesToMeters(10);
+	public static final double kGearRatio = 7.29;
+	public static final double kWheelRadiusMeters = Units.inchesToMeters(3.0);
 	public static final double MaxSafeVelocityMeters = Units.feetToMeters(2);
 	public static final double MaxSafeAccelerationMeters = Units.feetToMeters(2);
 	public static final double targetToCameraHeight = 5;
-  public static final double cameraAngle = 0;
-  
-  // Color Value Bounds
+	public static final double cameraAngle = 0;
+
+	// Color Value Bounds
 	public static double blueLowerBound = 106;
-  public static double blueUpperBound = 115;
+	public static double blueUpperBound = 115;
 
-  public static double redLowerBound = 65;
-  public static double redUpperBound = 81;
+	public static double redLowerBound = 65;
+	public static double redUpperBound = 81;
 
-  public static double greenLowerBound = 46;
-  public static double greenUpperBound = 60;
+	public static double greenLowerBound = 46;
+	public static double greenUpperBound = 60;
 
-  public static double yellowLowerBound = 91;
-  public static double yellowUpperBound = 98;
-  
-  // Constant Speeds
-  public static final double drivePercentLimit = .75;
- 
-  public static final double armPercentSpeed = 0.4;
-  
-  public static final double intakeVolts = 4;
-  public static final double shooterVolts = 12; // 9.25
+	public static double yellowLowerBound = 91;
+	public static double yellowUpperBound = 98;
 
-  public static final double intakeRPM = 50;
-  public static final double shooterRPM = 100;
+	// Constant Speeds
+	public static final double drivePercentLimit = .75;
 
-  public static final double conveyorVolts = 12;
+	public static final double armPercentSpeed = 0.4;
 
-  public static final double colorSwitchSpeed = 0.2;
-  public static final double colorSpeed = 0.1;
-  
-  // Field Measurements
-  public static final double cameraHeight = Units.inchesToMeters(30);
-  
-  public static final double ballTargetHeight = Units.inchesToMeters(81.25);
-  public static final double collectorTargetHeight = Units.inchesToMeters(11);
+	public static final double intakeVolts = 4;
+	public static final double shooterVolts = 12; // 9.25
 
-  public static final double cameraToBallTargetHeight = Units.inchesToMeters(51.25);
+	public static final double intakeRPM = 50;
+	public static final double shooterRPM = 100;
 
-  public static final double shooterDistanceFromTargetMeters = 5;
+	public static final double conveyorVolts = 12;
 
-  public static final double shooterRampUpTime = 1.0;
-  
+	public static final double colorSwitchSpeed = 0.2;
+	public static final double colorSpeed = 0.1;
+
+	// Field Measurements
+	public static final double cameraHeight = Units.inchesToMeters(30);
+
+	public static final double ballTargetHeight = Units.inchesToMeters(81.25);
+	public static final double collectorTargetHeight = Units.inchesToMeters(11);
+
+	public static final double cameraToBallTargetHeight = Units.inchesToMeters(51.25);
+
+	public static final double shooterDistanceFromTargetMeters = 5;
+
+	public static final double shooterRampUpTime = 1.0;
+
 }
